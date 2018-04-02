6b5b78fcf53b1c107ff5b1ebdc46ba9aa8efb74d
Ruaridh <ruaridhdunbar@gmail.com>
2018-04-02 11:36:29 +0100

    Homework completed after 1 to 1

--- pet_shop.rb
+++ pet_shop.rb
@@ -8,10 +8,11 @@ end
 
 def add_or_remove_cash(pet_shop, num)
   return pet_shop[:admin][:total_cash] += num
+  # return total_cash += num
 end
 
 def pets_sold(pet_shop)
- return pet_shop[:admin][:pets_sold]
+  return pet_shop[:admin][:pets_sold] #HELP these values dont make sense
 end
 
 def increase_pets_sold(pet_shop, num)
@@ -32,23 +33,21 @@ def pets_by_breed(pet_shop, breed)
    return pet_breeds
 end
 
-def find_pet_by_name(pet_shop, name)
+def find_pet_by_name(pet_shop, name) #PROBLEM
   for pet in pet_shop[:pets]
     if pet[:name] == name
-      return pet[:name]
-    else
-  return nil
+      return pet
     end
   end
+
+  return nil
 end
 
-def remove_pet_by_name(pet_shop, name)
+def remove_pet_by_name(pet_shop, name) #PROBLEM
   for pet in pet_shop[:pets]
-    name.delete(pet) if name == [:name]
-    end
-   for pet in pet_shop[:pets]
-     return nil if name != [:name]
-     end
+    pet_shop[:pets].delete(pet) if name == pet[:name]
+  end
+   return nil
   end
 
 def add_pet_to_stock(pet_shop, new_pet)
@@ -56,17 +55,12 @@ def add_pet_to_stock(pet_shop, new_pet)
   return pet_shop[:pets].count
 end
 
-def customer_pet_count(customers)
-  total_pets = 0
- for pet in customers[:pets]
-   total_pets += 1
- end
-  return total_pets
+def customer_pet_count(customer)
+  customer[:pets].length()
 end
 
 def add_pet_to_customer(customer, new_pet)
   customer[:pets] << new_pet
-  return customer[:pets]
 end
 
 def customer_can_afford_pet(customers, new_pet)
@@ -78,10 +72,10 @@ def customer_can_afford_pet(customers, new_pet)
 end
 
 def sell_pet_to_customer(pet_shop, pet, customer)
-   return add_pet_to_customer(customer, pet)
-   return customer_pet_count(customer)
-   return increase_pets_sold(pet_shop, 1)
-   return pets_sold(pet_shop)
+  if pet && customer_can_afford_pet(customer, pet)
+    #if pet means if pet = true
+   add_pet_to_customer(customer, pet)
+   increase_pets_sold(pet_shop, 1)
    add_or_remove_cash(pet_shop, pet[:price])
-   return total_cash
+ end
 end
